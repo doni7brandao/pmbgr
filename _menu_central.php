e4021a641d31ba5bc17caaa721286ca6877790ab
Donizete Brandão <donizetebrand@gmail.com>
2026-01-22 16:49:36 -0300

    Menu Central do Index
    Primeiro formato, aguardando futuras modificações.

---
+++ _menu_central.php
@@ -0,0 +1,192 @@
+<div id="content" class="col-sm-8 col-xs-12">
+<div class="modulos">
+<div class="panel panel-default no-border-radius">
+    <div class="panel-heading">
+        <h3 class="panel-title"><i class="fa fa-th p-r-5" aria-hidden="true"></i>&nbsp;&nbsp;Menu Principal</h3>
+    </div>
+    <div class="clearfix"></div>
+    <div class="panel-body no-p">
+  			<div class="x_content">
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+				    <a href="../transparencia/?p=ouvidoria" target="_self"/>
+					    <div class="coluna">
+						    <div class="text">
+							    <i class="fa fa-comments-o" aria-hidden="true"></i>
+							    <h3>Ouvidoria Geral</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+			        <a href="https://transparencia.appm.org.br/baixagrandedoribeiro/receitas" target="_blank"/>
+					    <div class="coluna">
+					        <div class="text">
+    						    <i class="fa fa-line-chart" aria-hidden="true"></i>
+    							<h3>Receitas</h3>
+							</div>
+					    </div>
+				    </a>
+				</div>
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+				    <a href="https://transparencia.appm.org.br/baixagrandedoribeiro/despesas" target="_blank"/>
+					<div class="coluna">
+						<div class="text">
+							<i class="fa fa-bar-chart" aria-hidden="true"></i>
+							<h3>Despesas</h3>
+						</div>
+					</div>
+					</a>
+				</div>
+
+				<div class="clearfix hidden-xs" style="height:0">&nbsp;</div>
+				
+				<div class="col col-xs-12 col-md-4 col-sm-4 md-sm-no wow fadeInUp">
+				    <a href="https://transparencia.appm.org.br/baixagrandedoribeiro/servidores" target="_blank"/>
+					    <div class="coluna">
+						    <div class="text">
+					            <i class="fa fa-users" aria-hidden="true">
+					            </i><h3>Servidores</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+				
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+		            <a href="../p/organograma" target="_self"/>
+					    <div class="coluna">
+						    <div class="text">
+    						    <i class="fa fa-sitemap" aria-hidden="true"></i>
+    				            <h3>Organograma</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+				
+				<div class="col col-xs-12 col-md-4 col-sm-4 md-sm-no wow fadeInUp">
+			        <a href="../transparencia/?p=secretarias" target="_self"/>
+					    <div class="coluna">
+						    <div class="text">
+					            <i class="fa fa-university" aria-hidden="true">
+					            </i><h3>Secretarias</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+				    <a href="https://transparencia.appm.org.br/baixagrandedoribeiro/diarias" target="_blank"/>
+					    <div class="coluna">
+						    <div class="text">
+						        <i class="fa fa-calendar-check-o" aria-hidden="true">
+						        </i><h3>Diárias</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+
+				<div class="clearfix hidden-xs" style="height:0">&nbsp;</div>
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 md-sm-no wow fadeInUp">
+				    <a href="../transparencia/?p=relatoriosfiscais" target="_self"/>
+					    <div class="coluna">
+						    <div class="text">
+				                <i class="fa fa-pie-chart" aria-hidden="true"></i>
+				                <h3>Relatórios RGF/RREO</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+				    <a href="../transparencia/?p=legismunicipal" target="_self"/>
+					    <div class="coluna">
+						    <div class="text">
+				                <i class="fa fa-balance-scale" aria-hidden="true"></i>
+				                <h3>Legislação Municipal</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+				
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+				    <a href="../transparencia/?p=extratos" target="_self"/>
+					    <div class="coluna">
+					        <span class="badge">NOVO</span>
+						    <div class="text">
+					            <i class="fa fa-wpforms" aria-hidden="true"></i>
+					            <h3>Extratos</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+				    <a href="../transparencia/?p=contratos" target="_self"/>
+					    <div class="coluna">
+						    <div class="text">
+					            <i class="fa fa-file-text-o" aria-hidden="true"></i>
+					            <h3>Contratos</h3>
+						    </div>
+					    </div>
+					</a>
+				</div>
+
+				<div class="clearfix hidden-xs" style="height:0">&nbsp;</div>
+
+				<div class="col col-xs-12 col-md-4 col-sm-4 wow fadeInUp">
+				    <a href="../transparencia/?p=licitacoes" target="_self"/>
+					    <div class="coluna">
+						    <div class="text">
+				                <i class="fa fa-gavel" aria-hidden="true"></i>
+				                <h3>Licitações</h3>
+						    </div>
+					    </div>
+			        </a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<div id="sidebar" class="col-sm-4 col-xs-12">
+    <div class="panel panel-default no-border-radius">
+        <div class="panel-heading">
+            <h3 class="panel-title"><span class="glyphicon glyphicon-open-file"></span>&nbsp;&nbsp;Nota Fiscal Eletrônica (NFS-e)</h3>
+        </div>
+        <div class="clearfix"></div>
+        <div class="panel-body no-p">
+      	    <div class="x_content">
+                <?= loadView('partials/_nota_fiscal.php'); ?>
+      		</div>
+      	</div>
+    </div>
+</div>
+<div id="sidebar" class="col-sm-4 col-xs-12">
+    <div class="panel panel-default no-border-radius">
+        <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-globe" aria-hidden="true"></i>&nbsp;&nbsp;Valor da Terra Nua - VTN</h3>
+        </div>
+        <div class="clearfix"></div>
+        <div class="panel-body no-p">
+      	    <div class="x_content">
+                <?= loadView('partials/_vtn.php'); ?>
+      		</div>
+      	</div>
+    </div>
+</div>
+<div id="sidebar" class="col-sm-4 col-xs-12">
+    <div class="panel panel-default no-border-radius">
+        <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-heartbeat" aria-hidden="true"></i>&nbsp;&nbsp;Sistema e-SUS</h3>
+        </div>
+        <div class="clearfix"></div>
+        <div class="panel-body no-p">
+      	    <div class="x_content">
+                <?= loadView('partials/_esus.php'); ?>
+      		</div>
+      	</div>
+    </div>
+</div>
